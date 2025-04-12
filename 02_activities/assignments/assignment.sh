f6593e5dbea62a1e8c60b8d00fa7c0a6efc65e70
Crystal <taltal@gmail.com>
2025-04-12 12:52:33 -0400

    Update assignment.sh from correct version

--- 02_activities/assignments/assignment.sh
+++ 02_activities/assignments/assignment.sh
@@ -24,7 +24,7 @@ unzip -q rawdata.zip
 mkdir data
 
 # 2. Move the ./rawdata directory to ./data/raw
-mv ./rawdata directory ./data/raw
+mv ./rawdata ./data/raw
 
 # 3. List the contents of the ./data/raw directory
 ls ./data/raw
@@ -34,10 +34,11 @@ mkdir ./data/processed
 mkdir ./data/processed/server_logs ./data/processed/user_logs ./data/processed/event_logs
 
 # 5. Copy all server log files (files with "server" in the name AND a .log extension) from ./data/raw to ./data/processed/server_logs
-cp ./data/raw/*server*.log ./data/processed/server_logs
+cp ./data/raw/*server* ./data/processed/server_logs
+
 # 6. Repeat the above step for user logs and event logs
-cp ./data/raw/*user*.log ./data/processed/server_logs
-cp ./data/raw/*event*log ./data/processed/server_logs
+cp ./data/raw/*user* ./data/processed/server_logs
+cp ./data/raw/*event* ./data/processed/server_logs
 
 # 7. For user privacy, remove all files containing IP addresses (files with "ipaddr" in the filename) from ./data/raw and ./data/processed/user_logs
 rm ./data/raw/*ipaddr* ./data/processed/user_logs/*ipaddr*
