56e032a00f0935f51c77178426a7d8a90eeaa048
Crystal <taltal@gmail.com>
2025-04-13 01:07:05 -0400

    changes on april13 0107

--- 02_activities/assignments/assignment.sh
+++ 02_activities/assignments/assignment.sh
@@ -38,7 +38,7 @@ cp ./data/raw/*server* ./data/processed/server_logs
 
 # 6. Repeat the above step for user logs and event logs
 cp ./data/raw/user.log ./data/processed/user_logs/
-cp ./data/raw/event.log ./data/processed/server_logs/
+cp ./data/raw/event.log ./data/processed/event_logs/
 
 # 7. For user privacy, remove all files containing IP addresses (files with "ipaddr" in the filename) from ./data/raw and ./data/processed/user_logs
 rm ./data/raw/*ipaddr* ./data/processed/user_logs/*ipaddr*
