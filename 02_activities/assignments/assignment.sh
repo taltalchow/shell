2e67a0ca03addf823536a1d0a1d7bd6f71912647
Crystal <taltal@gmail.com>
2025-04-13 12:26:16 -0400

    updates apeil13 1226

--- 02_activities/assignments/assignment.sh
+++ 02_activities/assignments/assignment.sh
@@ -37,8 +37,8 @@ mkdir ./data/processed/server_logs ./data/processed/user_logs ./data/processed/e
 cp ./data/raw/*server* ./data/processed/server_logs
 
 # 6. Repeat the above step for user logs and event logs
-cp ./data/raw/user.log ./data/processed/user_logs/
-cp ./data/raw/event.log ./data/processed/event_logs/
+cp ./data/raw/*user* ./data/processed/user_logs
+cp ./data/raw/*event* ./data/processed/event_logs
 
 # 7. For user privacy, remove all files containing IP addresses (files with "ipaddr" in the filename) from ./data/raw and ./data/processed/user_logs
 rm ./data/raw/*ipaddr* ./data/processed/user_logs/*ipaddr*
