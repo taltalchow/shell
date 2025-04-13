488f788136c8428318af40991e0fb540fec1da83
Crystal <taltal@gmail.com>
2025-04-13 01:03:54 -0400

    Update assignment.sh april 13

--- 02_activities/assignments/assignment.sh
+++ 02_activities/assignments/assignment.sh
@@ -37,15 +37,14 @@ mkdir ./data/processed/server_logs ./data/processed/user_logs ./data/processed/e
 cp ./data/raw/*server* ./data/processed/server_logs
 
 # 6. Repeat the above step for user logs and event logs
-cp ./data/raw/*user* ./data/processed/server_logs
-cp ./data/raw/*event* ./data/processed/server_logs
+cp ./data/raw/user.log ./data/processed/user_logs/
+cp ./data/raw/event.log ./data/processed/server_logs/
 
 # 7. For user privacy, remove all files containing IP addresses (files with "ipaddr" in the filename) from ./data/raw and ./data/processed/user_logs
 rm ./data/raw/*ipaddr* ./data/processed/user_logs/*ipaddr*
 
 # 8. Create a file named ./data/inventory.txt that lists all the files in the subfolders of ./data/processed
-mkdir ./data/processed/
-touch ./data/inventory.txt
+ls ./data/processed/* > ./data/inventory.txt
 
 ###########################################
 
